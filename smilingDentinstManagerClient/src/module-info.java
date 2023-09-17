65498b1c4eba825277a84b7a33e1348d2aff391f
Orlando Parente <143745421+OrlandoParente@users.noreply.github.com>
2023-09-17 17:14:38 +0200

    change name Management to Manager

---
+++ smilingDentinstManagerClient/src/module-info.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module smilingDentinstManagerClient {
+	
+	exports smilingDentinstManagerClient;
+	
+}
---
+++ smilingDentinstManagerClient/src/smilingDentinstManagerClient/Main.java
@@ -0,0 +1,9 @@
+package smilingDentinstManagerClient;
+
+public class Main {
+	
+	public static void main ( String [] args ) {
+		System.out.println("Start the Main Menu of the smiling dentist manager client ");
+	}
+	
+}
