10fdba608067d39330247702a187f11118a91713
Orlando Parente <143745421+OrlandoParente@users.noreply.github.com>
2023-09-19 19:20:12 +0200

    Add Customer and Medica History to database

--- SmilingDentistManagerServer/src/main/java/sdms/repository/DbManager.java
+++ SmilingDentistManagerServer/src/main/java/sdms/repository/DbManager.java
@@ -5,17 +5,14 @@ import java.sql.*;
 public class DbManager {
 
 	private static Connection conn;
-	private static boolean hasData;
+	private static boolean isConnectionOpen;
 	
-	// Singleton: voglio un solo oggetto di questa classe 
+	// Singleton: voglio un solo oggetto di questa classe #########################
 	private static DbManager dbManager;
 	
 	private DbManager() {
 		try {
-			
-			
 			this.getConnection();
-			
 			
 		} catch (ClassNotFoundException | SQLException e) {
 			//
@@ -31,33 +28,65 @@ public class DbManager {
 		return dbManager;
 		
 	}
+	// ############################################################################
 
 	private void getConnection() throws ClassNotFoundException, SQLException {
 			
+		// carica la classe JDBC
 		Class.forName("org.sqlite.JDBC");
 		conn = DriverManager.getConnection("jdbc:sqlite:database.db");
-		if( !hasData ) {
-			hasData = true;
+		
+		//
+		if( !isConnectionOpen ) {
+			
+			isConnectionOpen = true;
 			Statement state = conn.createStatement();
 			ResultSet res = state.executeQuery("Select name FROM sqlite_master WHERE type='table' AND name = 'data'");
 			
-			if( !res.next() )
-				System.out.println("Database assente");
+			// Se il database non esiste lo crea (vuoto)
+			if( !res.next() ) {
+				System.err.println("Database not foud ---> Creating new Database");
+				makeNewDatabase();
+			}
 			
-			// creazione relazione card
-			Statement createDatasTable = conn.createStatement();
-			createDatasTable.execute("CREATE TABLE customer("
-											+ "id int NOT NULL AUTO_INCREMENT,"
-											+ "name varchar(100),"
-											+ "surname varchar(100),"
-											+ "phone_number varchar(15),"
-											+ "phone_number_2 varchar(15),"
-											+ "e_mail varchar(100),"
-											+ "primary key(id)"
-											+ ")");
 		}
+	}
 	
-	
+	private void makeNewDatabase() throws SQLException {
+		
+		Statement createDatasTable = conn.createStatement();
+		
+		// creazione relazione customer
+		createDatasTable.execute("CREATE TABLE customer("
+										+ "id integer AUTO_INCREMENT," // non uso il cod_fiscale come id perché non so se il cliente è tenuto a rilasciarlo
+										+ "tax_id_code varchar(16)," // codice fiscale
+										+ "name varchar(100),"
+										+ "surname varchar(100),"
+										+ "birth_city varchar(100),"
+										+ "birth_city_province varchar(5),"  // sigla provincia
+										+ "birth_date date,"
+										+ "residence_street varchar(100),"
+										+ "residence_city varchar(100),"
+										+ "residence_province varchar(5),"   // sigla provincia
+										+ "phone_number varchar(15),"
+										+ "phone_number_2 varchar(15)," // Generalmente telefono di casa
+										+ "e_mail varchar(100),"
+										+ "PRIMARY KEY(id)"
+										+ ")");
+		
+		
+		// creazione relazione Medical History CIOE' Anamnesi
+		createDatasTable.execute("CREATE TABLE medical_history("
+										+ "id integer ," //
+										+ "id_customer integer," //
+										+ "type varchar(100),"   // [ anamnesi generale o odontoiatrica ]
+										+ "name varchar(100),"
+										+ "description varchar(100),"
+										+ "PRIMARY KEY(id AUTOINCREMENT),"
+										+ "FOREIGN KEY(id_customer) REFERENCES customer(id)"
+										+ ")");
+										
+		
 	}
 	
 }
