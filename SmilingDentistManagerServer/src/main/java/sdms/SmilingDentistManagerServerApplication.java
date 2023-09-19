9c0225b85389bc9611e4e2ce2b7f6c0ab8c1af58
Orlando Parente <143745421+OrlandoParente@users.noreply.github.com>
2023-09-19 11:26:34 +0200

    Created Smiling Dentist Manager Server with Spring Boot

---
+++ SmilingDentistManagerServer/src/main/java/sdms/SmilingDentistManagerServerApplication.java
@@ -0,0 +1,13 @@
+package sdms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmilingDentistManagerServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmilingDentistManagerServerApplication.class, args);
+	}
+
+}
---
+++ SmilingDentistManagerServer/src/main/java/sdms/controller/HomeController.java
@@ -0,0 +1,20 @@
+package sdms.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/")
+	public String index() {
+		return "index";
+	} 
+	
+	@RequestMapping("/prova")
+	@ResponseBody
+	public String prova() {
+		return "Pagina di prova";
+	} 
+}
