5d02cbafd370747153a4b1a74f6b5ae5126fc100
Orlando Parente <143745421+OrlandoParente@users.noreply.github.com>
2023-09-20 18:43:54 +0200

    Created file for easy insert samples data for database

---
+++ insert_example_data_in_db.sql
@@ -0,0 +1,6 @@
+/*
+INSERT INTO customer (tax_id_code, name, surname, birth_city, birth_city_province, birth_date, residence_street, residence_city, residence_province, phone_number, phone_number_2, e_mail )
+VALUES ('tax_id_code', 'name', 'surname', 'birth_city', 'birth_city_province', 'birth_date', 'residence_street', 'residence_city', 'residence_province', 'phone_number', 'phone_number_2', 'e_mail' )
+*/
+INSERT INTO customer (tax_id_code, name, surname, birth_city, birth_city_province, birth_date, residence_street, residence_city, residence_province, phone_number, phone_number_2, e_mail )
+VALUES ('1AAAAAAAAAAAAAAA', 'Mario', 'Rossi', 'Cosmo', 'RM', '1990-10-10', 'Leonardo Da Vinci', 'Cosenza', 'CS', '1234567890', '2345678901', 'mariorossi@email.it' )
