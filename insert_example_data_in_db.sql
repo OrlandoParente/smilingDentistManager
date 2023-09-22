4b97315d5aa33e8295628df25a0c6e1eae5fbe92
Orlando Parente <143745421+OrlandoParente@users.noreply.github.com>
2023-09-22 23:14:53 +0200

    updated example queries

--- insert_example_data_in_db.sql
+++ insert_example_data_in_db.sql
@@ -1,7 +1,7 @@
 /* CUSTOMERS */
 /*
-INSERT INTO customer (tax_id_code, name, surname, birth_city, birth_city_province, birth_date, residence_street, residence_city, residence_province, phone_number, phone_number_2, e_mail )
-VALUES ('tax_id_code', 'name', 'surname', 'birth_city', 'birth_city_province', 'birth_date', 'residence_street', 'residence_city', 'residence_province', 'phone_number', 'phone_number_2', 'e_mail');
+INSERT INTO customer (tax_id_code, name, surname, birth_city, birth_city_province, birth_date, residence_street, house_number, residence_city, residence_city_cap, residence_province, phone_number, phone_number_2, e_mail )
+VALUES ('tax_id_code', 'name', 'surname', 'birth_city', 'birth_city_province', 'birth_date', 'residence_street', 'house_number','residence_city', 'residence_city_cap', 'residence_province', 'phone_number', 'phone_number_2', 'e_mail');
 */
 
 /* EMPLOYEES */
@@ -15,8 +15,8 @@ VALUES ('name', 'description', 'cost');
 */
 
 /* CUSTOMERS */
-INSERT INTO customer (tax_id_code, name, surname, birth_city, birth_city_province, birth_date, residence_street, residence_city, residence_province, phone_number, phone_number_2, e_mail )
-VALUES ('1AAAAAAAAAAAAAAA', 'Mario', 'Rossi', 'Cosmo', 'RM', '1990-10-10', 'Leonardo Da Vinci', 'Cosenza', 'CS', '1234567890', '2345678901', 'mariorossi@email.it' );
+INSERT INTO customer (tax_id_code, name, surname, birth_city, birth_city_province, birth_date, residence_street, house_number, residence_city, residence_city_cap, residence_province, phone_number, phone_number_2, e_mail )
+VALUES ('1AAAAAAAAAAAAAAA', 'Mario', 'Rossi', 'Cosmo', 'RM', '1990-10-10', 'Leonardo Da Vinci', '2', 'Cosenza', '09091', 'CS', '1234567890', '2345678901', 'mariorossi@email.it' );
 
 
 /* EMPLOYEES */
