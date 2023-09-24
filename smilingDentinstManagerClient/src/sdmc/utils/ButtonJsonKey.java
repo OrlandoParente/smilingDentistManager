4780c656c035455e5eaf28873a2f5ec22d7f1300
Orlando Parente <143745421+OrlandoParente@users.noreply.github.com>
2023-09-24 12:32:57 +0200

    added new italian button names

--- smilingDentinstManagerClient/src/sdmc/utils/ButtonJsonKey.java
+++ smilingDentinstManagerClient/src/sdmc/utils/ButtonJsonKey.java
@@ -19,6 +19,8 @@ public final class ButtonJsonKey {
 	public final static String BTN_MAIN_MENU = "btnMainMenu";
 	public final static String BTN_SEARCH = "btnSearch";
 	public final static String BTN_ADD = "btnAdd";
-	
+	public final static String BTN_EDIT = "btnEdit";
+	public final static String BTN_DELETE = "btnDelete";
+	public final static String BTN_BACK = "btnBack";
 	
 }
