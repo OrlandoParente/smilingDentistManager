fb0223eec572cabb9315fd28e66a9e6deca45400
Orlando Parente <143745421+OrlandoParente@users.noreply.github.com>
2023-09-27 17:28:19 +0200

    Create ProfessionalRoleRestController.java

---
+++ SmilingDentistManagerServer/src/main/java/sdms/controller/api/ProfessionalRoleRestController.java
@@ -0,0 +1,110 @@
+package sdms.controller.api;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import sdms.model.ProfessionalRole;
+import sdms.repository.RepositoryInterface;
+
+@RestController
+public class ProfessionalRoleRestController {
+
+	// Spring si occupa di associare la giusta implementazione dell'interfaccia Repository
+	@Autowired
+	@Qualifier("mainRepository")
+	RepositoryInterface repository;
+	
+	
+	@GetMapping("/getProfessionalRoles")
+	public ArrayList<ProfessionalRole> getProfessionalRoles(){
+		
+		// Stampa di controllo
+		System.out.println("ProfessionalRoleRestController --> getProfessionalRoles ");
+		 
+		try {
+			
+			return repository.getProfessionalRoles();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return null;
+		
+	}
+
+	@PostMapping( value="/postProfessionalRole" , params= {"name"} )
+	public boolean postProfessionalRole( @RequestParam("name") String name ) {
+		
+		// Stampa di controllo
+		System.out.println("ProfessionalRoleRestController --> postProfessionalRoles -> params : " + name );
+		
+		try {
+			return repository.postProfessionalRole(name);
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
+		
+		return false;
+	}
+	
+	@PostMapping( value="/postProfessionalRole" , params= {"name", "description"} )
+	public boolean postProfessionalRole( @RequestParam("name") String name, @RequestParam("description") String description ) {
+		
+		// Stampa di controllo
+		System.out.println("ProfessionalRoleRestController --> postProfessionalRoles -> params : " + name + "," + description );
+		
+		try {
+			return repository.postProfessionalRole(name, description);
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
+		
+		return false;
+	}
+	
+	@PostMapping( value="/putProfessionalRoleById", params = {"id", "name", "description"} )
+	public boolean putProfessionalRoleById( @RequestParam("id") String id, @RequestParam("name") String name, 
+											@RequestParam("description") String description  ) {
+		
+		// Stampa di controllo
+		System.out.println("ProfessionalRoleRestController --> putProfessionalRolesById -> params : " + id + "," + name + "," + description );
+		
+		try {
+			return repository.putProfessionalRoleById( id, name,	description );
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
+		
+		return false;
+		
+	}
+	
+	@DeleteMapping( value="/deleteProfessionalRoleById", params = {"id"} )
+	public boolean deleteProfessionalRoleById( @RequestParam("id") String id  ) {
+		
+		// Stampa di controllo
+		System.out.println("ProfessionalRoleRestController --> deleteProfessionalRoleById --> id -> " + id );	
+		
+		try {
+			repository.deleteProfessionalRoleById( id );
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
+		
+		return false;
+	}
+
+}
