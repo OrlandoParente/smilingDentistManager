6d122c0a2f8732ec9274e4ffd09c2218fb54aabe
Orlando Parente <143745421+OrlandoParente@users.noreply.github.com>
2023-10-01 23:47:38 +0200

    Update module-info.java

--- smilingDentinstManagerClient/src/module-info.java
+++ smilingDentinstManagerClient/src/module-info.java
@@ -5,9 +5,17 @@ module smilingDentinstManagerClient {
 	
 	exports sdmc.main;
 	exports sdmc.utils;
+	exports sdmc.calendar;
+	exports sdmc.customer;
+	exports sdmc.employee;
+	exports sdmc.professional_role;
+	exports sdmc.combo_box_management;
+	exports sdmc.server_connection;
+	exports sdmc.settings;
 	
-	requires java.desktop;
+	requires transitive java.desktop;
 	requires transitive org.json;
 	requires java.net.http;
+	
 	
 }
