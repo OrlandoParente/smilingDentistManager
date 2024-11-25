260f03fc79993b4bd6e1d89e64e1db74c8f5e0d3
Orlando Parente <parenteorlando@yahoo.it>
2024-11-25 19:32:02 +0100

    added bean for Model Mapper

--- SmilingDentistManagerServer/src/main/java/sdms/SmilingDentistManagerServerApplication.java
+++ SmilingDentistManagerServer/src/main/java/sdms/SmilingDentistManagerServerApplication.java
@@ -1,7 +1,9 @@
 package sdms;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SmilingDentistManagerServerApplication {
@@ -10,4 +12,8 @@ public class SmilingDentistManagerServerApplication {
 		SpringApplication.run(SmilingDentistManagerServerApplication.class, args);
 	}
 
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
 }
