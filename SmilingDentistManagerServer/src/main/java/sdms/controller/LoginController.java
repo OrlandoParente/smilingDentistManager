f03c31ab58a300cf61872d6c2ea7320eda282666
Orlando Parente <parenteorlando@yahoo.it>
2024-12-12 11:48:59 +0100

    New login made with bootstrap

--- SmilingDentistManagerServer/src/main/java/sdms/controller/LoginController.java
+++ SmilingDentistManagerServer/src/main/java/sdms/controller/LoginController.java
@@ -1,7 +1,5 @@
 package sdms.controller;
 
-import javax.naming.AuthenticationException;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
