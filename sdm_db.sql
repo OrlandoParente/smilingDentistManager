857e65eb780bf731423e400463c4cf54ccda1d1d
Orlando Parente <parenteorlando@yahoo.it>
2024-12-16 22:13:31 +0100

    Update sdm_db.sql

--- sdm_db.sql
+++ sdm_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Creato il: Dic 10, 2024 alle 17:10
+-- Creato il: Dic 16, 2024 alle 22:12
 -- Versione del server: 10.4.32-MariaDB
 -- Versione PHP: 8.2.12
 
@@ -29,8 +29,8 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `appointment` (
   `id` bigint(20) NOT NULL,
-  `date` varchar(255) DEFAULT NULL,
-  `time` varchar(255) DEFAULT NULL,
+  `date` date DEFAULT NULL,
+  `time` time DEFAULT NULL,
   `id_customer` bigint(20) DEFAULT NULL,
   `id_doctor` bigint(20) DEFAULT NULL,
   `id_treatment` bigint(20) DEFAULT NULL,
@@ -54,8 +54,8 @@ INSERT INTO `appointment` (`id`, `date`, `time`, `id_customer`, `id_doctor`, `id
 (8, '2023-11-22', '12:00:00', 5, 1, 1, 0, '', ''),
 (9, '2023-12-20', '18:00:00', 6, 1, 1, 0, '', ''),
 (10, '2023-12-25', '12:00:00', 7, 1, 1, 1, '', ''),
-(11, '2024-11-21', '11:00', 5, NULL, NULL, 0, NULL, NULL),
-(13, '2024-12-14', '15:00', 3, NULL, NULL, 0, NULL, NULL);
+(11, '2024-11-21', '11:00:00', 5, NULL, NULL, 0, NULL, NULL),
+(13, '2024-12-14', '15:00:00', 3, NULL, NULL, 0, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -70,7 +70,7 @@ CREATE TABLE `customer` (
   `surname` varchar(255) DEFAULT NULL,
   `birth_city` varchar(255) DEFAULT NULL,
   `birth_city_province` varchar(255) DEFAULT NULL,
-  `birth_date` varchar(255) DEFAULT NULL,
+  `birth_date` date DEFAULT NULL,
   `residence_street` varchar(255) DEFAULT NULL,
   `house_number` varchar(255) DEFAULT NULL,
   `residence_city` varchar(255) DEFAULT NULL,
@@ -81,23 +81,24 @@ CREATE TABLE `customer` (
   `e_mail` varchar(255) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
   `permission` int(11) NOT NULL DEFAULT 0 COMMENT 'it defines what data the user can read, add and edit:\r\n0 = login not allowed; \r\n10 = admin.',
-  `salary` double DEFAULT NULL
+  `salary` double DEFAULT NULL,
+  `birth_date_string` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dump dei dati per la tabella `customer`
 --
 
-INSERT INTO `customer` (`id`, `tax_id_code`, `name`, `surname`, `birth_city`, `birth_city_province`, `birth_date`, `residence_street`, `house_number`, `residence_city`, `residence_city_cap`, `residence_province`, `phone_number`, `phone_number2`, `e_mail`, `password`, `permission`, `salary`) VALUES
-(1, '1AAAAAAAAAAAAAAA', 'Mario', 'Rossi', 'Cosmo', 'RM', '1990-10-10', 'Leonardo Da Vinci', '2', 'Cosenza', '09091', 'CS', '1234567890', '2345678901', 'mariorossi@email.it', NULL, 0, 0),
-(2, '1BBBBBBBBBBBBBBB', 'Giancarlo', 'Verdi', 'Roma', 'RM', '1980-10-10', 'Donatello', '11', 'Cosenza', '09088', 'CS', '9888776688', '2132762354', 'giancarloversi@email.it', NULL, 0, 0),
-(3, '1AAAAACCCAAAAAAA', 'Giovanni', 'Bianchi', 'Viterbo', 'RM', '1980-10-10', 'Michelangelo', '2', 'Cosenza', '09091', 'CS', '1234500000', '2349998901', 'giovannib@email.it', NULL, 0, 0),
-(4, '1BBBBBBBBBBDDDDD', 'Giusy', 'Verdi', 'Roma', 'RM', '1980-10-10', 'Donatello', '20', 'Cosenza', '11088', 'CS', '9888776111', '2132762111', 'giusy@email.it', NULL, 0, 0),
-(5, '1AAAAAAAAARRRRRE', 'Billy', 'Levi', 'Napoli', 'NA', '1990-10-10', 'Garibaldi', '5', 'Modena', '09091', 'MO', '1111167890', '2222228901', 'billy@email.it', NULL, 0, 0),
-(6, '1BBBRRRFBBBBBBBB', 'Leo', 'Biaggi', 'Roma', 'RM', '1990-10-10', 'Cavour', '11', 'Cosenza', '09088', 'CS', '9888776688', '2132762354', 'giro@email.it', NULL, 0, 0),
-(7, '1AKKKAAGAAAAAAAA', 'Milo', 'Isi', 'Cosmo', 'RM', '1994-07-10', 'Vittorio Emanuele', '2', 'Cosenza', '09091', 'CS', '1234567890', '2345678901', 'milo@email.it', NULL, 0, 0),
-(8, '12222BBHBBBBBBBB', 'Ciro', 'Adi', 'Roma', 'RM', '1989-07-10', 'Donatello', '11', 'Cosenza', '09088', 'CS', '9888776688', '2132762354', 'ciro@email.it', NULL, 0, 0),
-(33, '123456781234567', 'Giorgio', 'Vanni', 'Roma', 'FR', 'Latina', 'via garibaldi', '23', 'Milano', '20019', 'MI', '1212343423', NULL, 'g.vanni@email.com', NULL, 0, 0);
+INSERT INTO `customer` (`id`, `tax_id_code`, `name`, `surname`, `birth_city`, `birth_city_province`, `birth_date`, `residence_street`, `house_number`, `residence_city`, `residence_city_cap`, `residence_province`, `phone_number`, `phone_number2`, `e_mail`, `password`, `permission`, `salary`, `birth_date_string`) VALUES
+(1, '1AAAAAAAAAAAAAAA', 'Mario', 'Rossi', 'Cosmo', 'RM', '1990-10-10', 'Leonardo Da Vinci', '2', 'Cosenza', '09091', 'CS', '1234567890', '2345678901', 'mariorossi@email.it', NULL, 0, 0, NULL),
+(2, '1BBBBBBBBBBBBBBB', 'Giancarlo', 'Verdi', 'Roma', 'RM', '1980-10-10', 'Donatello', '11', 'Cosenza', '09088', 'CS', '9888776688', '2132762354', 'giancarloversi@email.it', NULL, 0, 0, NULL),
+(3, '1AAAAACCCAAAAAAA', 'Giovanni', 'Bianchi', 'Viterbo', 'RM', '1980-10-10', 'Michelangelo', '2', 'Cosenza', '09091', 'CS', '1234500000', '2349998901', 'giovannib@email.it', NULL, 0, 0, NULL),
+(4, '1BBBBBBBBBBDDDDD', 'Giusy', 'Verdi', 'Roma', 'RM', '1980-10-10', 'Donatello', '20', 'Cosenza', '11088', 'CS', '9888776111', '2132762111', 'giusy@email.it', NULL, 0, 0, NULL),
+(5, '1AAAAAAAAARRRRRE', 'Billy', 'Levi', 'Napoli', 'NA', '1990-10-10', 'Garibaldi', '5', 'Modena', '09091', 'MO', '1111167890', '2222228901', 'billy@email.it', NULL, 0, 0, NULL),
+(6, '1BBBRRRFBBBBBBBB', 'Leo', 'Biaggi', 'Roma', 'RM', '1990-10-10', 'Cavour', '11', 'Cosenza', '09088', 'CS', '9888776688', '2132762354', 'giro@email.it', NULL, 0, 0, NULL),
+(7, '1AKKKAAGAAAAAAAA', 'Milo', 'Isi', 'Cosmo', 'RM', '1994-07-10', 'Vittorio Emanuele', '2', 'Cosenza', '09091', 'CS', '1234567890', '2345678901', 'milo@email.it', NULL, 0, 0, NULL),
+(8, '12222BBHBBBBBBBB', 'Ciro', 'Adi', 'Roma', 'RM', '1989-07-10', 'Donatello', '11', 'Cosenza', '09088', 'CS', '9888776688', '2132762354', 'ciro@email.it', NULL, 0, 0, NULL),
+(33, '123456781234567', 'Giorgio', 'Vanni', 'Roma', 'FR', '1980-09-10', 'via garibaldi', '23', 'Milano', '20019', 'MI', '1212343423', NULL, 'g.vanni@email.com', NULL, 0, 0, NULL);
 
 -- --------------------------------------------------------
 
@@ -125,27 +126,28 @@ CREATE TABLE `employee` (
   `name` varchar(255) DEFAULT NULL,
   `surname` varchar(255) DEFAULT NULL,
   `title` varchar(255) DEFAULT NULL,
-  `birth_date` varchar(255) DEFAULT NULL,
+  `birth_date` date DEFAULT NULL,
   `salary` double DEFAULT 0,
   `phone_number` varchar(255) DEFAULT NULL,
   `phone_number_2` varchar(15) DEFAULT NULL,
   `e_mail` varchar(255) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
   `permission` int(11) NOT NULL DEFAULT 0 COMMENT 'it defines what data the user can read, add and edit:\r\n0 = login not allowed; \r\n10 = admin.',
-  `phone_number2` varchar(255) DEFAULT NULL
+  `phone_number2` varchar(255) DEFAULT NULL,
+  `birth_date_string` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dump dei dati per la tabella `employee`
 --
 
-INSERT INTO `employee` (`id`, `name`, `surname`, `title`, `birth_date`, `salary`, `phone_number`, `phone_number_2`, `e_mail`, `password`, `permission`, `phone_number2`) VALUES
-(1, 'Gianni', 'Marca', 'Dott.', '1979-01-01', 0, '1212122334', '', 'giannimarca@email.com', '1234', 0, NULL),
-(2, 'Eddy', 'Vetri', 'Sig.', '1979-02-10', 0, '1254122334', '', 'eddyvetri@email.com', '', 0, NULL),
-(3, 'Mila', 'Catogno', 'Sig.na', '1989-02-10', 0, '1254124314', '', 'mila@email.com', '', 0, NULL),
-(4, 'Giusy', 'Frocca', 'Dott.ssa', '1982-02-10', 0, '1254120301', '', 'giusyfrocca@email.com', '', 0, NULL),
-(5, 'Matteo', 'Gianni', 'Dott.', '1979-03-12', 0, '1222220301', '4445556634', 'mattih@email.com', '', 0, NULL),
-(18, 'Carlo', 'Conti', 'Dott.', '8/11/1980', 0, '1223341223', NULL, 'c.conti@email.it', '$2a$10$N.HjH/etkqHxsOY6UnNbhOpeGeFpWpVSwicvGtUN3DS42.yZtXWmu', 10, '3445234512');
+INSERT INTO `employee` (`id`, `name`, `surname`, `title`, `birth_date`, `salary`, `phone_number`, `phone_number_2`, `e_mail`, `password`, `permission`, `phone_number2`, `birth_date_string`) VALUES
+(1, 'Gianni', 'Marca', 'Dott.', '1979-01-01', 0, '1212122334', '', 'giannimarca@email.com', '1234', 0, NULL, NULL),
+(2, 'Eddy', 'Vetri', 'Sig.', '1979-02-10', 0, '1254122334', '', 'eddyvetri@email.com', '', 0, NULL, NULL),
+(3, 'Mila', 'Catogno', 'Sig.na', '1989-02-10', 0, '1254124314', '', 'mila@email.com', '', 0, NULL, NULL),
+(4, 'Giusy', 'Frocca', 'Dott.ssa', '1982-02-10', 0, '1254120301', '', 'giusyfrocca@email.com', '', 0, NULL, NULL),
+(5, 'Matteo', 'Gianni', 'Dott.', '1979-03-12', 0, '1222220301', '4445556634', 'mattih@email.com', '', 0, NULL, NULL),
+(18, 'Carlo', 'Conti', 'Dott.', '1980-08-11', 0, '1223341223', NULL, 'c.conti@email.it', '$2a$10$N.HjH/etkqHxsOY6UnNbhOpeGeFpWpVSwicvGtUN3DS42.yZtXWmu', 10, '3445234512', NULL);
 
 -- --------------------------------------------------------
 
@@ -159,7 +161,7 @@ CREATE TABLE `expense` (
   `id_employee` bigint(20) DEFAULT NULL,
   `id_dental_material` bigint(20) DEFAULT NULL,
   `amount` double NOT NULL,
-  `date` varchar(255) DEFAULT NULL,
+  `date` date DEFAULT NULL,
   `description` varchar(255) DEFAULT NULL,
   `tag` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
