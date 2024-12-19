f0420ef06ad554c39056eac04c1fc840561caf3f
Orlando Parente <parenteorlando@yahoo.it>
2024-12-19 22:30:31 +0100

    Update DashboardController.java

--- SmilingDentistManagerServer/src/main/java/sdms/controller/web/DashboardController.java
+++ SmilingDentistManagerServer/src/main/java/sdms/controller/web/DashboardController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import jakarta.servlet.http.HttpServletRequest;
 import sdms.util.WebClientCookieManager;
 
+// NOT USED, WE CAN DELETE THIS
+
 @Controller
 @RequestMapping("dashboard")
 public class DashboardController {
