4c1663f8b6a61bad4e17f9e5595453577617d176
Orlando Parente <parenteorlando@yahoo.it>
2024-12-30 11:29:14 +0100

    Some adjustments

--- SmilingDentistManagerServer/src/main/java/sdms/controller/web/employee/CustomerController.java
+++ SmilingDentistManagerServer/src/main/java/sdms/controller/web/employee/CustomerController.java
@@ -1,15 +1,25 @@
 package sdms.controller.web.employee;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.servlet.http.HttpServletRequest;
+import sdms.util.WebClientCookieManager;
 
 @Controller
 @RequestMapping("/employee/customer")
 public class CustomerController {
 
 	@GetMapping({"/customer","","/"})
-	public String mainCalendarPage() {
-		return("employee/customer/customer");
+	public String mainCalendarPage( HttpServletRequest request, Model model ) {
+		
+		// Set useful cookies --------------------------------------------------------------------------
+		WebClientCookieManager.setUsefulGlobalCookiesInTheModel(request, model);
+		// ---------------------------------------------------------------------------------------------
+		
+		
+		return("employee/customers/customers");
 	}
 }
--- SmilingDentistManagerServer/src/main/java/sdms/controller/web/employee/EmployeeController.java
+++ SmilingDentistManagerServer/src/main/java/sdms/controller/web/employee/EmployeeController.java
@@ -1,15 +1,24 @@
 package sdms.controller.web.employee;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.servlet.http.HttpServletRequest;
+import sdms.util.WebClientCookieManager;
 
 @Controller
 @RequestMapping("/employee/employee")
 public class EmployeeController {
 
 	@GetMapping({"/employee","","/"})
-	public String mainCalendarPage() {
-		return("employee/employee/employee");
+	public String mainCalendarPage( HttpServletRequest request, Model model ) {
+		
+		// Set useful cookies --------------------------------------------------------------------------
+		WebClientCookieManager.setUsefulGlobalCookiesInTheModel(request, model);
+		// ---------------------------------------------------------------------------------------------
+		
+		return("employee/employees/employees");
 	}
 }
