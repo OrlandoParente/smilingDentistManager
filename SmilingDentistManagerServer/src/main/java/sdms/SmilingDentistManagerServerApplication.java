da3eccb075d78ece148bf0963932d4c97df795c5
Orlando Parente <parenteorlando@yahoo.it>
2025-02-08 16:32:10 +0100

    Update SmilingDentistManagerServerApplication.java

--- SmilingDentistManagerServer/src/main/java/sdms/SmilingDentistManagerServerApplication.java
+++ SmilingDentistManagerServer/src/main/java/sdms/SmilingDentistManagerServerApplication.java
@@ -2,10 +2,8 @@ package sdms;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.util.AntPathMatcher;
 import sdms.util.DateAndTimeManager;
 
