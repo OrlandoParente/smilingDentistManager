638b063c117229a8a9022404e0f34646bff34f33
Orlando Parente <143745421+OrlandoParente@users.noreply.github.com>
2025-03-26 17:55:14 +0100

    Update EmailSettingsController.java

--- SmilingDentistManagerServer/src/main/java/sdms/controller/web/employee/EmailSettingsController.java
+++ SmilingDentistManagerServer/src/main/java/sdms/controller/web/employee/EmailSettingsController.java
@@ -1,14 +1,19 @@
 package sdms.controller.web.employee;
 
+import org.modelmapper.ModelMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import jakarta.servlet.http.HttpServletRequest;
+import sdms.dto.EmailSettingsDTO;
+import sdms.model.EmailSettings;
+import sdms.service.EmailSettingsServiceInterface;
 import sdms.service.EmployeeServiceInterface;
 import sdms.util.WebClientCookieManager;
 
@@ -21,6 +26,12 @@ public class EmailSettingsController {
 	@Autowired
 	EmployeeServiceInterface employeeService;
 	
+	@Autowired
+	EmailSettingsServiceInterface emailSettingsService;
+	
+	@Autowired
+	ModelMapper modelMapper;
+	
 	@GetMapping( value={"", "/", "/email-settings", "/email-settings/"} )
 	public String getEmailSettingsPage( HttpServletRequest request, Model model ) {
 		
@@ -29,6 +40,11 @@ public class EmailSettingsController {
 		
 		// Set useful cookies in the model
 		WebClientCookieManager.setUsefulGlobalCookiesInTheModel(request, model, employeeService);
+		
+		EmailSettingsDTO emailAccount =  modelMapper.map( emailSettingsService.getEmailSettings(), EmailSettingsDTO.class );
+		
+		
+		model.addAttribute("emailAccount", emailAccount);
 		
 		return "employee/email-settings/email-settings";
 	}
