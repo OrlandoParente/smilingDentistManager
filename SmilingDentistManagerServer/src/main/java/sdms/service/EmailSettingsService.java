5825d4d42f25e38900912826245acf0f75996b62
Orlando Parente <143745421+OrlandoParente@users.noreply.github.com>
2025-03-26 17:54:53 +0100

    Updated EmailSettingsService

--- SmilingDentistManagerServer/src/main/java/sdms/service/EmailSettingsService.java
+++ SmilingDentistManagerServer/src/main/java/sdms/service/EmailSettingsService.java
@@ -1,5 +1,49 @@
 package sdms.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import sdms.model.EmailSettings;
+import sdms.repository.EmailSettingsRepository;
+
+@Service
 public class EmailSettingsService implements EmailSettingsServiceInterface {
+
+	@Autowired
+	private EmailSettingsRepository repository;
+	
+	@Override
+	public EmailSettings getEmailSettings() {
+		
+		return repository.findById( EmailSettings.DEFAULT_UNIQUE_ID ).orElse(null);
+	}
+	
+	@Override
+	public void saveSettings(String host, int port, String username, String password, boolean enableAuth,
+			boolean enableTLS, boolean enableSSL) {
+		
+		EmailSettings emailSettings = new EmailSettings();
+		emailSettings.setHost(host);
+		emailSettings.setPort(port);
+		emailSettings.setUsername(username);
+		emailSettings.setPassword(password);
+		emailSettings.setEnableAuth(enableAuth);
+		emailSettings.setEnableTLS(enableTLS);
+		emailSettings.setEnableSSL(enableSSL);
+		
+		// We want always only ONE email account in the database
+		emailSettings.setId( EmailSettings.DEFAULT_UNIQUE_ID );
+		
+		repository.save( emailSettings );
+	}
+
+	@Override
+	public void saveSettings(EmailSettings emailSettings) {
+		
+		// We want always only ONE email account in the database
+		emailSettings.setId( EmailSettings.DEFAULT_UNIQUE_ID );
+		
+		repository.save( emailSettings );		
+	}
 
 }
--- SmilingDentistManagerServer/src/main/java/sdms/service/EmailSettingsServiceInterface.java
+++ SmilingDentistManagerServer/src/main/java/sdms/service/EmailSettingsServiceInterface.java
@@ -1,5 +1,11 @@
 package sdms.service;
 
+import sdms.model.EmailSettings;
+
 public interface EmailSettingsServiceInterface  {
 
+	public EmailSettings getEmailSettings();
+	
+	public void saveSettings( EmailSettings emailSettings );
+	public void saveSettings( String host, int port, String username, String password, boolean enableAuth, boolean enableTLS, boolean enableSSL);
 }
