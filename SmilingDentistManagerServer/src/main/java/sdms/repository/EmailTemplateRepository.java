f2bb207680e78edb49d001f46744004f1bd06bac
Orlando Parente <143745421+OrlandoParente@users.noreply.github.com>
2025-03-27 14:01:20 +0100

    Update EmailTemplateRepository.java

--- SmilingDentistManagerServer/src/main/java/sdms/repository/EmailTemplateRepository.java
+++ SmilingDentistManagerServer/src/main/java/sdms/repository/EmailTemplateRepository.java
@@ -1,9 +1,12 @@
 package sdms.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import sdms.model.EmailTemplate;
 
 public interface EmailTemplateRepository extends JpaRepository<EmailTemplate, Long> {
 
+	Optional<EmailTemplate> findByType(String type);
 }
