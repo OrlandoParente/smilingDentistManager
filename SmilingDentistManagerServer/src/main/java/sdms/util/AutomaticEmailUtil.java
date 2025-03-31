9fff78316bb39acedbcf408c06d449f4aea3e8db
Orlando Parente <143745421+OrlandoParente@users.noreply.github.com>
2025-03-31 19:28:23 +0200

    Create AutomaticEmailUtil.java

---
+++ SmilingDentistManagerServer/src/main/java/sdms/util/AutomaticEmailUtil.java
@@ -0,0 +1,32 @@
+package sdms.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AutomaticEmailUtil {
+
+	// default values ################
+	public final static Integer AUTOMATIC_EMAIL_DISABLED = -1;
+	public final static Integer AUTOMATIC_EMAIL_ONE_WEEK = 7;
+	public final static Integer AUTOMATIC_EMAIL_THREE_MONTHS = 84;
+	public final static Integer AUTOMATIC_EMAIL_FOUR_MONTHS = 112;
+	public final static Integer AUTOMATIC_EMAIL_SIX_MONTHS = 168;
+	public final static Integer AUTOMATIC_EMAIL_ONE_YEAR = 365;
+	// ###############################
+	
+	public static List<Integer> getAutomaticEmailIntervals(){
+		
+		List<Integer> intervals = new ArrayList<>();
+		
+		intervals.add(AUTOMATIC_EMAIL_DISABLED);
+		intervals.add(AUTOMATIC_EMAIL_ONE_WEEK);
+		intervals.add(AUTOMATIC_EMAIL_THREE_MONTHS);
+		intervals.add(AUTOMATIC_EMAIL_FOUR_MONTHS);
+		intervals.add(AUTOMATIC_EMAIL_SIX_MONTHS);
+		intervals.add(AUTOMATIC_EMAIL_ONE_YEAR);
+		
+		return intervals;
+	}
+	
+	
+}
