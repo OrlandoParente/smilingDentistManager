3f37667c6b38f0ad60a3210788644c5e20d98c76
Orlando Parente <143745421+OrlandoParente@users.noreply.github.com>
2025-04-10 09:34:54 +0200

    Created js for update some orthopantomogram fields

--- SmilingDentistManagerServer/src/main/resources/static/js/customers/customer-details/orthopantomograms/upload-orthopantomogram.js
+++ SmilingDentistManagerServer/src/main/resources/static/js/customers/customer-details/orthopantomograms/upload-orthopantomogram.js
@@ -11,66 +11,66 @@
         const errMsgText =  document.getElementById("errMsgTextOrthopantomograms");
 
 
-        btnUploadOrthopantomogram.addEventListener( "click" , function () {
+        btnUploadOrthopantomogram.addEventListener( "click" , function ( event ) {
 
-             // prevent default behavior 
-        event.preventDefault();
+          // prevent default behavior 
+          event.preventDefault();
 
-        // Reset messages
-        resetMessages();
+          // Reset messages
+          resetMessages();
 
-        // Recovery data from the Put Customer Form
-        const formDataUploadOrthopantomogram = new FormData(formUploadOrthopantomogram);
+          // Recovery data from the Put Customer Form
+          const formDataUploadOrthopantomogram = new FormData(formUploadOrthopantomogram);
 
-        // NOTE: For use @ResponseBody on the server I have to sent data in json format
-        // const formDataObj = Object.fromEntries(formDataPutCustomer.entries());
-        // const jsonFormData = JSON.stringify(formDataObj);
+          // NOTE: For use @ResponseBody on the server I have to sent data in json format
+          // const formDataObj = Object.fromEntries(formDataPutCustomer.entries());
+          // const jsonFormData = JSON.stringify(formDataObj);
 
-        console.log( "------------------------------> " + formDataUploadOrthopantomogram.get('idCustomer')  );
-        console.log( "------------------------------> " + formDataUploadOrthopantomogram.get('orthopantomogram')  );
-        console.log( "------------------------------> " + formDataUploadOrthopantomogram.get('orthopantomogram').name  );
+          // console.log( "------------------------------> " + formDataUploadOrthopantomogram.get('idCustomer')  );
+          // console.log( "------------------------------> " + formDataUploadOrthopantomogram.get('orthopantomogram')  );
+          // console.log( "------------------------------> " + formDataUploadOrthopantomogram.get('orthopantomogram').name  );
 
-        // check if mandatory fields are not empty
-        if( ! formDataUploadOrthopantomogram.get('orthopantomogram') 
-            || ! formDataUploadOrthopantomogram.get('orthopantomogram').name ){
+          // check if mandatory fields are not empty
+          if( ! formDataUploadOrthopantomogram.get('orthopantomogram') 
+              || ! formDataUploadOrthopantomogram.get('orthopantomogram').name ){
 
-           errMsg.style.display = "block";
-           errMsgText.innerText = "Choose a file to upload";
+            errMsg.style.display = "block";
+            errMsgText.innerText = "Choose a file to upload";
 
-           return;
-        }
+            return;
+          }
 
-        // control print
-        console.log("POST url: " + formUploadOrthopantomogram.action);
+          // control print
+          console.log("POST url: " + formUploadOrthopantomogram.action);
 
-        // Do post AJAX request
-        fetch( formUploadOrthopantomogram.action, { 
-                  method:'POST', 
-                  body:formDataUploadOrthopantomogram
-                  // NOTE: For use @ResponseBody on the server I have to sent data in json format
-                  // headers: {
-                  //    'Content-Type': 'application/json' // Imposta l'intestazione del contenuto come JSON
-                  // },
-                  // body: jsonFormData
-                  } )
-          .then( response => {
-            if( response.status == 200 ) {
-              
-              window.location.reload();
+          // Do post AJAX request
+          fetch( formUploadOrthopantomogram.action, { 
+                    method:'POST', 
+                    body:formDataUploadOrthopantomogram
+                    // NOTE: For use @ResponseBody on the server I have to sent data in json format
+                    // headers: {
+                    //    'Content-Type': 'application/json' // Imposta l'intestazione del contenuto come JSON
+                    // },
+                    // body: jsonFormData
+                    } )
+            .then( response => {
+              if( response.status == 200 ) {
+                
+                window.location.reload();
 
-              console.log( 'Upload Orthopantomogram success');
+                console.log( 'Upload Orthopantomogram success');
 
-              alert(Success);
-              // this need if you want chain another ".then"
-              // return response.json();
+                alert(Success);
+                // this need if you want chain another ".then"
+                // return response.json();
 
-            } else {
-              errMsg.style.display = "block";
-              errMsgText.innerText = "Failed to upload Orthopantomogram ";
-            }
-          }).catch( error => {
-            console.log('Error: ' + error ); 
-          });
+              } else {
+                errMsg.style.display = "block";
+                errMsgText.innerText = "Failed to upload Orthopantomogram ";
+              }
+            }).catch( error => {
+              console.log('Error: ' + error ); 
+            });
 
         });
 
