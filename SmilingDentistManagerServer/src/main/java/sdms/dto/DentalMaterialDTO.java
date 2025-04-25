827e4254913e914d6a24f4f79fc6f4f1ee30da8e
Orlando Parente <143745421+OrlandoParente@users.noreply.github.com>
2025-04-25 20:39:53 +0200

    Update DentalMaterialDTO.java

--- SmilingDentistManagerServer/src/main/java/sdms/dto/DentalMaterialDTO.java
+++ SmilingDentistManagerServer/src/main/java/sdms/dto/DentalMaterialDTO.java
@@ -2,7 +2,7 @@ package sdms.dto;
 
 public class DentalMaterialDTO {
 
-	private long id;
+	private Long id;
 	
 	private String name;
 	private int quantity;
@@ -12,11 +12,11 @@ public class DentalMaterialDTO {
 	public DentalMaterialDTO() {}
 
 
-	public long getId() {
+	public Long getId() {
 		return id;
 	}
 
-	public void setId(long id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 
