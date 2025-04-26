3cf821f4a9b2ed34b48c2bf9852304887bbae9b0
Orlando Parente <143745421+OrlandoParente@users.noreply.github.com>
2025-04-26 19:19:42 +0200

    Update EmailSettings.java

--- SmilingDentistManagerServer/src/main/java/sdms/model/EmailSettings.java
+++ SmilingDentistManagerServer/src/main/java/sdms/model/EmailSettings.java
@@ -13,7 +13,7 @@ public class EmailSettings {
 	
 	@Id
 	@GeneratedValue( strategy = GenerationType.IDENTITY )
-	private long id;
+	private Long id;
 	
 	private String host;
 	private int port;
@@ -37,11 +37,11 @@ public class EmailSettings {
 	
 	// GETTERS AND SETTERS
 	
-	public long getId() {
+	public Long getId() {
 		return id;
 	}
 
-	public void setId(long id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 
