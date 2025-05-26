32b81f04cf4a69d25c07ad9d1432a9a51a8b7c92
Orlando Parente <143745421+OrlandoParente@users.noreply.github.com>
2025-05-26 23:36:50 +0200

    Create ExpenseServiceTest.java

---
+++ SmilingDentistManagerServer/src/test/java/sdms/service/ExpenseServiceTest.java
@@ -0,0 +1,35 @@
+package sdms.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/*
+ * 	// READ ------------------------------------------------------
+	public Expense getExpenseById( long id );
+	public List<Expense> getExpenses();
+	public List<Expense> getExpensesByCustomerId( long idCustomer );
+	public List<Expense> getExoensesByEmployeeId( long idEmployee );
+	public List<Expense> getExpensesByDentalMaterialId( long idDentalMaterial );
+	public List<String> getExpenseTags();
+
+	// CREATE ----------------------------------------------------
+	public void postExpense( Expense expense );
+	// actually we don't need this, but I use this in the code. First edit the code and then I can delete this
+	public void postDentalMaterialPurchase( long idDentalMaterial , double amountExpense, int quantity, LocalDate date, String tag );
+
+	// UPDATE ----------------------------------------------------
+	public void putExpense( Expense expense );
+	
+	// DELETE ----------------------------------------------------
+	public void deleteExpense( long id );
+ */
+
+class ExpenseServiceTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
