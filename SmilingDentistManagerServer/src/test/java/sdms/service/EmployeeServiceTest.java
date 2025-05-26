9b9e0f0b136e87656d1b36e103feb3346b2d6bdb
Orlando Parente <143745421+OrlandoParente@users.noreply.github.com>
2025-05-26 23:36:26 +0200

    Create EmployeeServiceTest.java

---
+++ SmilingDentistManagerServer/src/test/java/sdms/service/EmployeeServiceTest.java
@@ -0,0 +1,42 @@
+package sdms.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/*
+ * 	public long getLastCustomerId();
+	
+	public List<Employee> getEmployees();
+	public List<Employee> getEmployeesByName( String name );
+	public List<Employee> getEmployeesBySurname( String surname );
+	public List<Employee> getEmployeesByProfessionalRoleName( String professionalRoleName );
+	public List<Employee> getEmployeesByProfessionalRoleId( long professionalRoleId );
+	public List<Employee> getEmployeesByPartialKeyWordOverAllFields( String keyWord );
+	
+	public List<ProfessionalRole> getEmployeeProfessionalRole( long idEmployee );
+	
+	public Employee getEmployeeById( Long id );
+	public Employee getEmployeeByEMail( String eMail );
+//	
+//	// title e.g. Dott. , Dott.ssa, Sig. , Sig.ra , Sig.na
+//	boolean postEmployee( String name, String surname, String title, String phone_number ) throws SQLException;
+//	boolean postEmployee( String name, String surname, String title, String birth_date, String phone_number, String phone_number_2, String e_mail ) throws SQLException;
+//	
+	public void postEmployee( Employee employee );
+//	boolean putEmployeeById( String id, String name, String surname, String title, String birth_date, 
+//							String phone_number, String phone_number_2, String e_mail ) throws SQLException;
+//
+	public void putEmployee( Employee employee );
+
+	public void deleteEmployeeById( Long id );
+ */
+
+class EmployeeServiceTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
