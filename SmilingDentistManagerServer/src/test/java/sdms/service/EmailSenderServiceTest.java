30bd0721d38eb949637276cf6e257f8fd0aae7e6
Orlando Parente <143745421+OrlandoParente@users.noreply.github.com>
2025-05-26 22:17:34 +0200

    Create EmailSenderServiceTest.java

---
+++ SmilingDentistManagerServer/src/test/java/sdms/service/EmailSenderServiceTest.java
@@ -0,0 +1,21 @@
+package sdms.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/*
+ * 	public void sendEmail(String from, String to, String subject, String text);
+	public void sendHtmlEmail( String from, String to, String subject, String htmlBody );
+	
+//	public void sendResetPasswordEmail();
+ */
+
+class EmailSenderServiceTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
