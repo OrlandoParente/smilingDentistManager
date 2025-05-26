47dbfcd3264c0c39772a8e11655e548422c8b4f9
Orlando Parente <143745421+OrlandoParente@users.noreply.github.com>
2025-05-26 23:36:20 +0200

    Create EmailTemplateServiceTest.java

---
+++ SmilingDentistManagerServer/src/test/java/sdms/service/EmailTemplateServiceTest.java
@@ -0,0 +1,36 @@
+package sdms.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+
+/*
+ * 	public EmailTemplate getEmailTemplateById( Long id );
+	public EmailTemplate getEmailTemplateByType( String type );
+	
+	// Use default tags
+	public EmailTemplate getEmailTemplateRecall();
+	
+	
+	public List<EmailTemplate> getAllEmailTemplates();
+	
+	// Use default tags
+	public void saveRecallEmailTemplate( EmailTemplate emailTemplate );
+	
+	// Generic post/put Email Template
+	public void postEmailTemplate( EmailTemplate emailTemplate );
+	public void putEmailTemplate( EmailTemplate emailTemplate );
+	
+	public void deleteEmailTemplateByType( String type );
+	public void deleteEmailTemplateById( Long id );
+ */
+
+class EmailTemplateServiceTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
