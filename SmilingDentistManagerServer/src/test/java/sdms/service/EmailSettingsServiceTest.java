c23f419b240c29531861ce846a473e1f581b5392
Orlando Parente <143745421+OrlandoParente@users.noreply.github.com>
2025-05-26 22:17:57 +0200

    Create EmailSettingsServiceTest.java

---
+++ SmilingDentistManagerServer/src/test/java/sdms/service/EmailSettingsServiceTest.java
@@ -0,0 +1,23 @@
+package sdms.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import sdms.model.EmailSettings;
+
+/*
+ * 	public EmailSettings getEmailSettings();
+	
+	public void saveSettings( EmailSettings emailSettings );
+	public void saveSettings( String host, int port, String username, String password, boolean enableAuth, boolean enableTLS, boolean enableSSL);
+ */
+
+class EmailSettingsServiceTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
