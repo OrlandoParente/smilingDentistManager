9d64c67843316c01a16cfcebcf3a9c46841e8b8e
Orlando Parente <143745421+OrlandoParente@users.noreply.github.com>
2025-05-26 22:14:57 +0200

    Create AppointmentServiceTest.java

---
+++ SmilingDentistManagerServer/src/test/java/sdms/service/AppointmentServiceTest.java
@@ -0,0 +1,65 @@
+package sdms.service;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+
+/*
+ * 	// Gestione Appuntamenti --------------------------------
+	public Appointment getAppointmentById( long id );
+	
+	public List<Appointment> getAppointments();
+	
+	public List<Appointment> getAppointmentsByCustomerId( long customerId );
+	
+	public List<Appointment> getAppointmentsByDoctorId( long doctorId );
+	
+	public List<String> getInvoiceNumbersByCustomerId( long customerId );
+
+	public void postAppointment( Appointment appointment );
+	// per registrare un appuntamento ancora non svolto
+	// boolean postAppointment( String date, String time, String id_customer, String id_doctor, String id_treatment, String note) throws SQLException;
+	
+	// per registrare un appuntamento già svolto
+	// boolean postAppointment( String date, String time, String id_customer, String id_doctor, String id_treatment, String bill_number, String note) throws SQLException;
+	
+	// per registrare un appuntamento potendo scegliere i valori di tutti i campi
+	//boolean postAppointment( String date, String time, String id_customer, String id_doctor, String id_treatment, 
+	//		int is_done, String bill_number, String note);
+	
+	// set is_done = 1
+	public void putSetAppointmentDoneById( long id );
+	
+	// set is_done = 0
+	public void putUnsetAppointmentDoneById( long id );
+
+	public void putAppointment( Appointment appointment );
+	
+	// Volendo se ne può fare a meno, basta il putAppointment
+	// però dato che questa funzionalità è prevista, così si alleggerisce il controller
+	public void putAppointmentBillNumberById( long id, String invoiceNumber );
+	
+	// Volendo se ne può fare a meno, basta il putAppointment
+	// però dato che questa funzionalità è prevista, così si alleggerisce il controller
+	public void putAppointmentNoteById( long id , String notes );
+	
+	// Volendo se ne può fare a meno, basta il putAppointment
+	// però dato che questa funzionalità è prevista, così si alleggerisce il controller
+	public void putAppointmentTreatmentById( long id, long idTreatment );
+	
+	// PATCH REQUESTS -----------------------------
+	public void updateTeethList( long id, String teeth );
+	// --------------------------------------------
+	
+	public void deleteAppointmentById( long id );
+	// ------------------------------------------------------
+ */
+
+class AppointmentServiceTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
