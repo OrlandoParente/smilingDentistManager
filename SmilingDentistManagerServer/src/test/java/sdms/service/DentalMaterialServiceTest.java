2cac93257b338c39fc4eee98c2ab764f6f3eff00
Orlando Parente <143745421+OrlandoParente@users.noreply.github.com>
2025-05-26 22:17:27 +0200

    Create DentalMaterialServiceTest.java

---
+++ SmilingDentistManagerServer/src/test/java/sdms/service/DentalMaterialServiceTest.java
@@ -0,0 +1,34 @@
+package sdms.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/*
+ * 	
+	// CREATE ----------------------------------------------------
+	public void postDentalMaterial( DentalMaterial dentalMaterial );
+	
+	// READ ------------------------------------------------------
+	public DentalMaterial getDentalMaterialById( long id );
+	public List<DentalMaterial> getDentalMaterials();
+	
+	// UPDATE ----------------------------------------------------
+	public void putDentalMaterial( DentalMaterial dentalMaterial );
+	public void increaseDentalMaterialQuantity( long idDentalMaterial, int quantity );
+	public void decreaseDentalMaterialQuantity( long idDentalMaterial, int quantity );
+	
+	// DELETE ----------------------------------------------------
+	public void deleteDentalMaterial( long id );
+	
+}
+ */
+
+class DentalMaterialServiceTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
