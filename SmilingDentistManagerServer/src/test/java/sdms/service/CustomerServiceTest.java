e2cf88a755219d6cb17a95686dc27089f390e206
Orlando Parente <143745421+OrlandoParente@users.noreply.github.com>
2025-05-26 22:17:23 +0200

    Create CustomerServiceTest.java

---
+++ SmilingDentistManagerServer/src/test/java/sdms/service/CustomerServiceTest.java
@@ -0,0 +1,47 @@
+package sdms.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/*
+ * 	public long getLastCustomerId();
+	
+	public Customer getCustomerById( long id );
+	public Customer getCustomerByEMail( String eMail );
+	
+	public List<Customer> getCustomers();
+	public List<Customer> getCustomersByPartialKeyWordOverAllFields( String key_word );
+	
+
+
+//	// inserimento dati essenziali del cliente
+//	boolean postCustomer( String name , String surname ,String phone_number ) throws SQLException;
+//	
+//	boolean postCustomer( String tax_id_code , String name , String surname , String birth_city , 
+//			String birth_city_province, String birth_date, String residence_street, String house_number, 
+//			String residence_city , String residence_city_cap , String residence_province, String phone_number ,
+//			String phone_number_2, String e_mail )
+//		    throws SQLException;4+
+	public void postCustomer( Customer customer );
+//	
+//	boolean putCustomerById( String id, String tax_id_code , String name , String surname , String birth_city , 
+//			String birth_city_province, String birth_date, String residence_street, String house_number, 
+//			String residence_city , String residence_city_cap , String residence_province, String phone_number ,
+//			String phone_number_2, String e_mail )
+//		    throws SQLException;
+//	
+	public void putCustomer( Customer customer );
+
+	public void deleteCustomer( long id );
+	
+ */
+
+class CustomerServiceTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
