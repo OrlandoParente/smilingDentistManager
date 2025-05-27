104200dd5eff4f214fbe8749063c1d7dff556b83
Orlando Parente <143745421+OrlandoParente@users.noreply.github.com>
2025-05-27 19:41:28 +0200

    Create HasProfessionalRoleServiceTest.java

---
+++ SmilingDentistManagerServer/src/test/java/sdms/service/HasProfessionalRoleServiceTest.java
@@ -0,0 +1,32 @@
+package sdms.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/*
+ * 	// CREATE
+	public void postLinkEmployeeToProfessionalRole( HasProfessionalRole hasProfessionalRole );
+	
+	// READ
+	public List<ProfessionalRole> getProfessionalRolesByEmployee( long idEmployee );
+	public List<Employee> getEmployeesByProfessionalRole( long idProfessionalRole );
+	public List<Employee> getEmployeesByProfessionalRoleName( String professionalRoleName );
+	
+	// UPDATE
+	
+	
+	// DELETE
+	public void deleteLinkEmployeeWithProfessionalRole( long idEmployee, long idProfessionalRole );
+	public void deleteLinkEmployeeWithProfessionalRole( long id );
+
+ */
+
+class HasProfessionalRoleServiceTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
