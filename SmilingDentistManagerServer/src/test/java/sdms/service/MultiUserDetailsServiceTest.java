31503ef55ebc0d214e5af8bf5f642a206865f2bc
Orlando Parente <143745421+OrlandoParente@users.noreply.github.com>
2025-05-27 19:41:33 +0200

    Create MultiUserDetailsServiceTest.java

---
+++ SmilingDentistManagerServer/src/test/java/sdms/service/MultiUserDetailsServiceTest.java
@@ -0,0 +1,18 @@
+package sdms.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+// 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+class MultiUserDetailsServiceTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
