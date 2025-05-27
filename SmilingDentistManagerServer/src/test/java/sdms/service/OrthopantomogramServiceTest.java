8605ff850cc2fe6d1b1a36729c27a7bbbfbd8ae5
Orlando Parente <143745421+OrlandoParente@users.noreply.github.com>
2025-05-27 19:41:37 +0200

    Create OrthopantomogramServiceTest.java

---
+++ SmilingDentistManagerServer/src/test/java/sdms/service/OrthopantomogramServiceTest.java
@@ -0,0 +1,32 @@
+package sdms.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/*
+ * 	public Orthopantomogram getOrthopantomogramById( Long id );
+	
+	public List<Orthopantomogram> getOrthopantomogramsByCustomerAndFilename( Long idCustomer, String filename );
+	public List<Orthopantomogram> getOrthopantomograms();
+	public List<Orthopantomogram> getOrthopantomogramsByCustomer( Long idCustomer );
+
+	public void uploadOrthopantomogram( Long idCustomer, MultipartFile orthopantomogram, String format );
+	public void uploadOrthopantomogram( Long idCustomer,String folderPath, MultipartFile orthopantomogram, String format );
+	
+	public void uploadOrthopantomogram( Long idCustomer, MultipartFile orthopantomogram, String format, LocalDate date );
+	public void uploadOrthopantomogram( Long idCustomer, String folderPath, MultipartFile orthopantomogram, String format, LocalDate date );
+	
+	public void putOrthopantomogram( Orthopantomogram orthopantomogram );
+	
+	public void deleteOrthopantomogram( Long id );
+ */
+
+class OrthopantomogramServiceTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
