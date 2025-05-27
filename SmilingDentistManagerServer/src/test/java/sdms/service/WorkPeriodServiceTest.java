40c1578c591813ed6bc09e59256721fa728c46a9
Orlando Parente <143745421+OrlandoParente@users.noreply.github.com>
2025-05-27 19:42:17 +0200

    Create WorkPeriodServiceTest.java

---
+++ SmilingDentistManagerServer/src/test/java/sdms/service/WorkPeriodServiceTest.java
@@ -0,0 +1,30 @@
+package sdms.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+
+/*
+ * 	// CREATE ---------------------------------------------------------------
+	public void postWorkPeriod( WorkPeriod workPeriod );
+	
+	// READ -----------------------------------------------------------------
+	WorkPeriod getWorkPeriodById( long id );
+	List<WorkPeriod> getWorkPeriodsByEmployee(Employee employee);
+	
+	// UPDATE ---------------------------------------------------------------
+	public void putWorkPeriod( WorkPeriod workPeriod );
+		
+	// DELETE ---------------------------------------------------------------
+	public void deleteWorkPeriodById( long id );
+ */
+
+class WorkPeriodServiceTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
