43160b5f8c800e9d613e7b0165b4de20a58d0b72
Orlando Parente <143745421+OrlandoParente@users.noreply.github.com>
2025-05-27 19:41:31 +0200

    Create MedicalHistoryServiceTest.java

---
+++ SmilingDentistManagerServer/src/test/java/sdms/service/MedicalHistoryServiceTest.java
@@ -0,0 +1,42 @@
+package sdms.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import sdms.model.MedicalHistory;
+
+/*
+ * 	// CREATE
+	public void postMedicalHistory( MedicalHistory medicalHistory );
+	
+	// READ
+	public List<MedicalHistory> getMedicalHistories();
+	public List<MedicalHistory> getMedicalsHistoryByCustomerId( long idCustomer );
+	public MedicalHistory getMedicalHistoryById( Long id );
+	
+	public List<String> getMedicalHistoryTypes();
+	public List<String> getMedicalHistoryCategories();
+	
+	public List<MedicalHistory> getMedicalsHistoryByType( String type );
+	public List<MedicalHistory> getMedicalsHistoryByCategory( String category );
+	public List<MedicalHistory> getMedicalsHistoryByTypeAndCategory( String type, String category );
+	
+	// UPDATE
+	public void putMedicalHistory( MedicalHistory medicalHistory );
+	
+	// DELETE
+	public void deleteMedicalHistoryById( Long id );
+	
+ */
+
+class MedicalHistoryServiceTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
