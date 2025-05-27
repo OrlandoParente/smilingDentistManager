72210916ad62e20a8b3c7c3fc8fec18672f7c312
Orlando Parente <143745421+OrlandoParente@users.noreply.github.com>
2025-05-27 19:41:40 +0200

    Create ProfessionalRoleServiceTest.java

---
+++ SmilingDentistManagerServer/src/test/java/sdms/service/ProfessionalRoleServiceTest.java
@@ -0,0 +1,39 @@
+package sdms.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/*
+ * //	// Gestione Professional Role ---------------------------
+	public ProfessionalRole getProfessionalRoleById( long id );
+	
+//	ArrayList<ProfessionalRole> getProfessionalRoles() throws SQLException;
+	public List<ProfessionalRole> getProfessionalRoles();
+	
+//	ArrayList<ProfessionalRole> getProfessionalRolesAssociatedToIdEmployee( String id_employee ) throws SQLException;
+	public List<ProfessionalRole> getProfessionalRolesAssociatedToIdEmployee( long idEmployee );
+	
+//	
+//	boolean postProfessionalRole( String name ) throws SQLException;
+//	boolean postProfessionalRole( String name, String description ) throws SQLException;
+	public void postProfessionalRole( ProfessionalRole professionalRole );
+	
+//	boolean putProfessionalRoleById( String id, String name, String description ) throws SQLException;
+	public void putProfessionalRole( ProfessionalRole professionalRole );
+	
+//	boolean deleteProfessionalRoleById( String id ) throws SQLException;	
+	public void deleteProfessionalRoleById( Long id );
+	
+//	// ------------------------------------------------------
+//	
+ */
+
+class ProfessionalRoleServiceTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
