aabbd4a0991272b63efd8e47b3778e8c942465b4
Orlando Parente <143745421+OrlandoParente@users.noreply.github.com>
2025-05-27 19:41:54 +0200

    Create TreatmentServiceTest.java

---
+++ SmilingDentistManagerServer/src/test/java/sdms/service/TreatmentServiceTest.java
@@ -0,0 +1,29 @@
+package sdms.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+
+/*
+ * 	public Treatment getTreatmentById( long id );
+	public List<Treatment> getTreatments();
+	public List<Treatment> getTreatmentsByCustomerId( long idCustomer );
+	public List<Treatment> getTreatmentsByBillNumber( String billNumber );
+	
+	public void postTreatment( Treatment treatment );
+	
+	public void putTreatment(Treatment treatment);
+	
+	public void deleteTreatmentById( long id );
+	
+ */
+
+class TreatmentServiceTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
