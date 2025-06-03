dbcee0d4d2ac652a3b30ab86d93bba40d6bf6c4e
Orlando Parente <143745421+OrlandoParente@users.noreply.github.com>
2025-06-03 17:00:32 +0200

    Update WorkPeriodServiceTest.java

--- SmilingDentistManagerServer/src/test/java/sdms/service/WorkPeriodServiceTest.java
+++ SmilingDentistManagerServer/src/test/java/sdms/service/WorkPeriodServiceTest.java
@@ -22,9 +22,9 @@ import org.junit.jupiter.api.Test;
 
 class WorkPeriodServiceTest {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+//	@Test
+//	void test() {
+//		fail("Not yet implemented");
+//	}
 
 }
