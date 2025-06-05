241fa1a4a5445840d5cd2431e218ec7ce530fd25
Orlando Parente <143745421+OrlandoParente@users.noreply.github.com>
2025-06-05 19:06:16 +0200

    Update DentalMaterialServiceTest.java

--- SmilingDentistManagerServer/src/test/java/sdms/service/DentalMaterialServiceTest.java
+++ SmilingDentistManagerServer/src/test/java/sdms/service/DentalMaterialServiceTest.java
@@ -2,7 +2,9 @@ package sdms.service;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockitoAnnotations;
 
 /*
  * 	
@@ -26,6 +28,13 @@ import org.junit.jupiter.api.Test;
 
 class DentalMaterialServiceTest {
 
+	
+	// enable mochito annotations
+	@BeforeEach
+	public void setUp() {
+		MockitoAnnotations.openMocks(this);
+	}
+	
 //	@Test
 //	void test() {
 //		fail("Not yet implemented");
