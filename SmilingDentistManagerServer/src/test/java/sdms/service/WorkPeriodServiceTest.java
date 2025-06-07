e215991365aa56bfe6e42cf9724ffcd70197e0a2
Orlando Parente <143745421+OrlandoParente@users.noreply.github.com>
2025-06-07 20:08:18 +0200

    Update WorkPeriodServiceTest.java

--- SmilingDentistManagerServer/src/test/java/sdms/service/WorkPeriodServiceTest.java
+++ SmilingDentistManagerServer/src/test/java/sdms/service/WorkPeriodServiceTest.java
@@ -1,10 +1,18 @@
 package sdms.service;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoSettings;
+
+import sdms.model.WorkPeriod;
+import sdms.repository.WorkPeriodRepository;
 
 
 /*
@@ -24,7 +32,13 @@ import org.mockito.MockitoAnnotations;
 
 class WorkPeriodServiceTest {
 
-	// enable mochito annotations
+	@Mock
+	private WorkPeriodRepository workPeriodRepository;
+	
+	@InjectMocks
+	private WorkPeriodServiceInterface workPeriodService;
+	
+	// enable mockito annotations
 	@BeforeEach
 	public void setUp() {
 		MockitoAnnotations.openMocks(this);
@@ -34,5 +48,38 @@ class WorkPeriodServiceTest {
 //	void test() {
 //		fail("Not yet implemented");
 //	}
+	
+	// CREATE ---------------------------------------------------------------
+	// public void postWorkPeriod( WorkPeriod workPeriod );
+	
+	@Test
+	public void testPostWorkPeriod() {
+		
+		// simulate database ----------------------------------
+		WorkPeriod workPeriod = new WorkPeriod();
+		Long id = 1L;
+		workPeriod.setId( id );
+		
+		// ----------------------------------------------------
+		
+		// test -----------------------------------------------
+		workPeriodService.postWorkPeriod(workPeriod);
+		// ----------------------------------------------------
+		
+		// check ----------------------------------------------
+		verify( workPeriodRepository, times(1) ).save( workPeriod );
+		// ----------------------------------------------------
+		
+	}
+	
+	// READ -----------------------------------------------------------------
+	// WorkPeriod getWorkPeriodById( long id );
+	// List<WorkPeriod> getWorkPeriodsByEmployee(Employee employee);
+	
+	// UPDATE ---------------------------------------------------------------
+	// public void putWorkPeriod( WorkPeriod workPeriod );
+		
+	// DELETE ---------------------------------------------------------------
+	// public void deleteWorkPeriodById( long id );
 
 }
