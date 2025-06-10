67607758489a794588301d804348815a354d11cd
Orlando Parente <143745421+OrlandoParente@users.noreply.github.com>
2025-06-10 16:12:44 +0200

    Update EmailSettingsServiceInterface.java

--- SmilingDentistManagerServer/src/main/java/sdms/service/EmailSettingsServiceInterface.java
+++ SmilingDentistManagerServer/src/main/java/sdms/service/EmailSettingsServiceInterface.java
@@ -7,5 +7,7 @@ public interface EmailSettingsServiceInterface  {
 	public EmailSettings getEmailSettings();
 	
 	public void saveSettings( EmailSettings emailSettings );
+	
+	// <---------- SHOULD I DELETE THIS?, Should be enough the one with the only object ? 
 	public void saveSettings( String host, int port, String username, String password, boolean enableAuth, boolean enableTLS, boolean enableSSL);
 }
