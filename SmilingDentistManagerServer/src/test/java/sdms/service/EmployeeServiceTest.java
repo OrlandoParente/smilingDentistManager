fdd0518eba366ac8a7672c3468cc56f6531cca34
Orlando Parente <143745421+OrlandoParente@users.noreply.github.com>
2025-06-15 18:14:49 +0200

    Update EmployeeServiceTest.java

--- SmilingDentistManagerServer/src/test/java/sdms/service/EmployeeServiceTest.java
+++ SmilingDentistManagerServer/src/test/java/sdms/service/EmployeeServiceTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.MockitoAnnotations;
 
 /*
- * 	public long getLastCustomerId();
+  	public long getLastCustomerId();
 	
 	public List<Employee> getEmployees();
 	public List<Employee> getEmployeesByName( String name );
@@ -46,5 +46,31 @@ class EmployeeServiceTest {
 //	void test() {
 //		fail("Not yet implemented");
 //	}
+	
+	  	// public long getLastCustomerId();
+		
+		// public List<Employee> getEmployees();
+		// public List<Employee> getEmployeesByName( String name );
+		// public List<Employee> getEmployeesBySurname( String surname );
+		// public List<Employee> getEmployeesByProfessionalRoleName( String professionalRoleName );
+		// public List<Employee> getEmployeesByProfessionalRoleId( long professionalRoleId );
+		// public List<Employee> getEmployeesByPartialKeyWordOverAllFields( String keyWord );
+		
+		// public List<ProfessionalRole> getEmployeeProfessionalRole( long idEmployee );
+		
+		// public Employee getEmployeeById( Long id );
+		// public Employee getEmployeeByEMail( String eMail );
+		
+//		// title e.g. Dott. , Dott.ssa, Sig. , Sig.ra , Sig.na
+//		boolean postEmployee( String name, String surname, String title, String phone_number ) throws SQLException;
+//		boolean postEmployee( String name, String surname, String title, String birth_date, String phone_number, String phone_number_2, String e_mail ) throws SQLException;
+		
+		// public void postEmployee( Employee employee );
+//		boolean putEmployeeById( String id, String name, String surname, String title, String birth_date, 
+//								String phone_number, String phone_number_2, String e_mail ) throws SQLException;
+	
+		// public void putEmployee( Employee employee );
+
+		// public void deleteEmployeeById( Long id );
 
 }
