cf4d71c457cb80c97777724a5c81d2ce4513ae61
Orlando Parente <143745421+OrlandoParente@users.noreply.github.com>
2025-06-15 18:13:05 +0200

    Update EmailSenderServiceTest.java

--- SmilingDentistManagerServer/src/test/java/sdms/service/EmailSenderServiceTest.java
+++ SmilingDentistManagerServer/src/test/java/sdms/service/EmailSenderServiceTest.java
@@ -1,10 +1,14 @@
 package sdms.service;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockitoAnnotations;
+
+import sdms.model.EmailSettings;
 
 /*
  * 	public void sendEmail(String from, String to, String subject, String text);
@@ -26,5 +30,25 @@ class EmailSenderServiceTest {
 //	void test() {
 //		fail("Not yet implemented");
 //	}
-
+	
+	
+	// public void sendEmail(String from, String to, String subject, String text);
+	
+	public void testSendEmail() {
+		
+		// Simulate the database ----------------------------
+		// --------------------------------------------------
+							
+		// test ---------------------------------------------
+		// --------------------------------------------------
+							
+		// check --------------------------------------------
+		// --------------------------------------------------	
+	}
+	
+	// public void sendHtmlEmail( String from, String to, String subject, String htmlBody );
+	
+	// public void sendResetPasswordEmail();
+	
+	
 }
