0464f335800712a2bfe303ccf594fdee005622ee
Orlando Parente <143745421+OrlandoParente@users.noreply.github.com>
2025-06-16 10:35:36 +0200

    Update EmployeeServiceTest.java

--- SmilingDentistManagerServer/src/test/java/sdms/service/EmployeeServiceTest.java
+++ SmilingDentistManagerServer/src/test/java/sdms/service/EmployeeServiceTest.java
@@ -1,7 +1,15 @@
 package sdms.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+
+import sdms.repository.EmployeeRepository;
 
 /*
   	public long getLastCustomerId();
@@ -33,6 +41,12 @@ import org.mockito.MockitoAnnotations;
 
 class EmployeeServiceTest {
 
+	@Mock
+	EmployeeRepository employeeRepository;
+	
+	@InjectMocks
+	EmployeeService employeeService;
+	
 	// enable mochito annotations
 	@BeforeEach
 	public void setUp() {
@@ -44,30 +58,47 @@ class EmployeeServiceTest {
 //		fail("Not yet implemented");
 //	}
 	
-	  	// public long getLastCustomerId();
-		
-		// public List<Employee> getEmployees();
-		// public List<Employee> getEmployeesByName( String name );
-		// public List<Employee> getEmployeesBySurname( String surname );
-		// public List<Employee> getEmployeesByProfessionalRoleName( String professionalRoleName );
-		// public List<Employee> getEmployeesByProfessionalRoleId( long professionalRoleId );
-		// public List<Employee> getEmployeesByPartialKeyWordOverAllFields( String keyWord );
-		
-		// public List<ProfessionalRole> getEmployeeProfessionalRole( long idEmployee );
-		
-		// public Employee getEmployeeById( Long id );
-		// public Employee getEmployeeByEMail( String eMail );
-		
-//		// title e.g. Dott. , Dott.ssa, Sig. , Sig.ra , Sig.na
-//		boolean postEmployee( String name, String surname, String title, String phone_number ) throws SQLException;
-//		boolean postEmployee( String name, String surname, String title, String birth_date, String phone_number, String phone_number_2, String e_mail ) throws SQLException;
-		
-		// public void postEmployee( Employee employee );
-//		boolean putEmployeeById( String id, String name, String surname, String title, String birth_date, 
-//								String phone_number, String phone_number_2, String e_mail ) throws SQLException;
+	// This method is for keep compatibility with desktop client
+	// public long getLastCustomerId();
 	
-		// public void putEmployee( Employee employee );
-
-		// public void deleteEmployeeById( Long id );
+	@Test
+	public void testGetLastCustomerId() {
+		
+		// Simulate the database ----------------------------
+		Long lastId = 1L;
+		
+		when( employeeRepository.findMaxId() ).thenReturn( lastId );
+		// --------------------------------------------------
+							
+		// test ---------------------------------------------
+		Long result = employeeService.getLastCustomerId();
+		// --------------------------------------------------
+							
+		// check --------------------------------------------
+		assertEquals(result, lastId);
+		// --------------------------------------------------	
+	}
+	
+	// public List<Employee> getEmployees();
+	// public List<Employee> getEmployeesByName( String name );
+	// public List<Employee> getEmployeesBySurname( String surname );
+	// public List<Employee> getEmployeesByProfessionalRoleName( String professionalRoleName );
+	// public List<Employee> getEmployeesByProfessionalRoleId( long professionalRoleId );
+	// public List<Employee> getEmployeesByPartialKeyWordOverAllFields( String keyWord );
+	// public List<ProfessionalRole> getEmployeeProfessionalRole( long idEmployee );
+	// public Employee getEmployeeById( Long id );
+	// public Employee getEmployeeByEMail( String eMail );
+	
+	//		// title e.g. Dott. , Dott.ssa, Sig. , Sig.ra , Sig.na
+	//		boolean postEmployee( String name, String surname, String title, String phone_number ) throws SQLException;
+	//		boolean postEmployee( String name, String surname, String title, String birth_date, String phone_number, String phone_number_2, String e_mail ) throws SQLException;
+	
+	// public void postEmployee( Employee employee );
+	//		boolean putEmployeeById( String id, String name, String surname, String title, String birth_date, 
+	//								String phone_number, String phone_number_2, String e_mail ) throws SQLException;
+	
+	// public void putEmployee( Employee employee );
+	
+	// public void deleteEmployeeById( Long id );
 
 }
